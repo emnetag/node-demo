91baa154b62228f622f4d04da31fcde842578e31
Emnet Gossaye <emnetg@uw.edu>
2014-11-24 12:02:49 -0800

    in class work using node.js

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -8,7 +8,37 @@
 var express = require('express');
 var _db;
 
+function getAllTasks(req, res, next) {
+    _db.all('select rowid, title, done from tasks where done=0', function(err, rows) {
+        if (err) {
+            return next(err);
+        }
 
+        res.json(rows);
+    });
+}
+
+function insertTask(req, res, next) {
+    if (!req.body.title || 0 == req.body.title.trim().length) {
+        return next({statusCode: 400, message: 'All tasks must have a title'});
+    }
+
+    _db.run('insert into tasks (title, done) values (?, 0)', req.body.title, function(err) {
+        if (err) {
+            return next(err)
+        }
+        res.json({rowid: this.lastID});
+    });
+}
+
+function updateTask(req, res, next) {
+    _db.run('update tasks set done=? where rowid=?', req.body.done, req.params.id, function(err) {
+        if (err) {
+            return next(err)
+        }
+        res.json({rowsAffected: this.changes});
+    });
+}
 
 module.exports.Router = function(db) {
     //hold on to database reference
@@ -20,10 +50,13 @@ module.exports.Router = function(db) {
     //add routes for:
 
     //GET /tasks (gets all undone tasks)
-    //POST /tasks (inserts new task)
+    router.get('/tasks', getAllTasks);
 
+    //POST /tasks (inserts new task)
+    router.post('/tasks', insertTask);
     //GET /tasks/:id (gets a particular task)
     //PUT /tasks/:id (updates a particular task)
+    router.put('/tasks/:id', updateTask);
     //DELETE /tasks/:id (deletes a particular task)
 
     return router;
